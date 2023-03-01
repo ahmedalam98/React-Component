e8e9a6b1fae97299daf7d6720b07a156978e18a2
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-01 14:17:38 +0200

    Add animation

--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -19,7 +19,13 @@ class RollDice extends Component {
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     //set state with new rolls
     this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
+    //wait one second, then set rolling to false
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
+
   render() {
     return (
       <div className="RollDice">
@@ -27,7 +33,9 @@ class RollDice extends Component {
           <Die face={this.state.die1} rolling={this.state.rolling} />
           <Die face={this.state.die2} rolling={this.state.rolling} />
         </div>
-        <button onClick={this.roll}>Roll Dice !</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        </button>
       </div>
     );
   }
